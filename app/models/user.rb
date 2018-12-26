e57eb25a99af0b9fd037062cb8a28b8eb7333caf
sho.koma <aaa@gmail.com>
2018-12-26 11:22:06 +0000

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save {self.email.downcase!} #保存する前に全ての文字を小文字に変換
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, #emailを表す正規表現
+            uniqueness: {case_sensitive: false} #大文字と小文字の区別をせず同じものと見なす
+  has_secure_password
+end
